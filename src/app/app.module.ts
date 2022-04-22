067e6f9be4929f5ad6079b9fc8382aed3ae95058
yassineelhoubi <elhoubiyassine@gmail.com>
2022-04-22 06:27:16 +0000

    Async configuration with mongoose

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from 'src/user/user.module';
-
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
+import { ConfigModule } from '../config/config.module';
+import { ConfigService } from '../config/config.service';
 @Module({
   imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const options: MongooseModuleOptions = {
+          uri: configService.get('DATABASE_URI'),
+        };
+        return options;
+      },
+      inject: [ConfigService],
+    }),
     UserModule
   ],
 })
-export class AppModule {}
+export class AppModule { }
