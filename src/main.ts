043172cd1dc65d843f10269602a1908bb5fdeb35
yassineelhoubi <elhoubiyassine@gmail.com>
2022-04-29 13:53:00 +0000

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
     forbidNonWhitelisted: true
   }))
-  await app.listen(3000);
+  await app.listen(3030);
 }
 bootstrap();
